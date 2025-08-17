62cc33543a2c5418e68e7bfd2225964fb6d1afda
Eng. Marwan Abd ElAziz <marwan.aboelenin@aast.edu>
2025-08-17 20:21:09 +0300

    upload src new code

--- src/test.sh
+++ src/test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#comment1
-EXPECTED= "hello, test!"
+# comment1
+
+EXPECTED="hello, test!"
 OUTPUT=$(node -e "console.log(require('./src/app')('test'))")
 
 if [ "$OUTPUT" == "$EXPECTED" ]; then
-echo "test passed"
-exit 0
+  echo "test passed"
+  exit 0
 else
-echo "test failed"
-exit 1
+  echo "test failed"
+  exit 1
 fi
